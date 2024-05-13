bd4e66f614bbadc947e08dcb59e2ed57671eed39
Mohamad Meaari <123135462+MiariHub@users.noreply.github.com>
2024-05-13 17:47:16 +0300

    project.py

--- project.py
+++ project.py
@@ -20,7 +20,17 @@ df = pd.read_csv("Real estate valuation data set.csv")
 menu = st.sidebar.radio("Menu", ["Home", "Dashboard","Prediction"])
 
 if menu == "Home":
+   col1, col2, col3 = st.columns(3)
+
+   with col1:
+    st.write(' ')
+
+   with col2:
    st.image("pngegg (8).png", caption="Real Estate Prediction")
+
+   with col3:
+    st.write(' ')
+
    st.markdown("""
 <div style="text-align: justify;">
     Welcome to our state-of-the-art platform designed to predict real estate prices with precision. 
