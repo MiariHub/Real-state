e5f3467ec3d0ed18ce296c882324093ff71fe378
Mohamad Meaari <123135462+MiariHub@users.noreply.github.com>
2024-05-14 02:29:35 +0300

    project.py

--- project.py
+++ project.py
@@ -18,7 +18,7 @@ df = pd.read_csv("Real estate valuation data set.csv")
 
 
 
-menu = st.sidebar.radio("Menu", ["Home", "Dashboard","Prediction"و"About"])
+menu = st.sidebar.radio("Menu", ["Home", "Dashboard","Prediction","About"])
 if menu == "Home":
     col1, col2, col3 = st.columns(3)
     
