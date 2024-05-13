81ed8fba248708dd5de99c5b72308ed4ec6ca9c3
Mohamad Meaari <123135462+MiariHub@users.noreply.github.com>
2024-05-14 02:39:30 +0300

    project.py

--- project.py
+++ project.py
@@ -8,7 +8,7 @@ import joblib
 
 st.set_page_config(page_title="RealEstate!!!", page_icon=":bar_chart:",layout="wide")
 
-st.markdown("<h1 style='text-align: center; color: white;font-family: "Times New Roman"'>House Price Prediction Using AI</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center; color: white;font-family: Times New Roman'>House Price Prediction Using AI</h1>", unsafe_allow_html=True)
 
 
 #st.title("House Price Prediction Using AI")
