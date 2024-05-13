75a08b2531b85402a276a4a03c730b00df8d029f
Mohamad Meaari <123135462+MiariHub@users.noreply.github.com>
2024-05-13 17:50:45 +0300

    project.py

--- project.py
+++ project.py
@@ -19,7 +19,7 @@ df = pd.read_csv("Real estate valuation data set.csv")
 
 menu = st.sidebar.radio("Menu", ["Home", "Dashboard","Prediction"])
 if menu == "Home":
-    col1, col2, col3 = st.columns(3)
+col1, col2, col3 = st.columns(3)
     
     with col1:
         st.write(' ')
